dd090874a04e79eb18ab93865bf191b5dfdc39bd
ilyaqwerty <topmastertop97@gmail.com>
2019-03-01 01:03:57 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+
+    entry: './src/index.js',
+
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'public')
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ],
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: "html-loader"
+                  }
+                ]
+              }   
+        ]
+    }
+}
